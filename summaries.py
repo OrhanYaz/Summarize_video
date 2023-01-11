c4cb24fa57492c24a07cd0a7b9df148012955db6
orhan yazar <orhan.yazar@konecranes.com>
2023-01-11 12:16:01 +0100

    added truncation for bart large model bec ause of token limit

--- summaries.py
+++ summaries.py
@@ -11,9 +11,17 @@ url = "https://www.youtube.com/watch?v=Lpp9bHtPAN0"
 
 audio_file = download_youtube_vide(url)
 
-audio_url = upload_audiofile_to_assemblyai(audio_file)
-
-endpoint = "https://api.assemblyai.com/v2/transcript"
-
-
+audio_file = 'Lpp9bHtPAN0.mp3'
 import whisper
+
+model = whisper.load_model("base")
+result = model.transcribe(audio_file)
+print(result["text"])
+
+
+from transformers import pipeline
+
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn", tokenizer="facebook/bart-large-cnn", truncation=True)
+print(summarizer(result["text"], max_length=1024, min_length=50, do_sample=False))
+
+

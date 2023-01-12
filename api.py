02e56208d0b19d1b8c3eda8b13649f92b80b011b
orhan yazar <orhan.yazar@konecranes.com>
2023-01-12 22:26:46 +0100

    updated api to take url

--- api.py
+++ api.py
@@ -9,13 +9,13 @@ def home_page():
     return 'welcome to the youtube summary api'
 
 
-@app.route('/get_summary/<url>', methods=['GET'])
+@app.route('/get_summary/<path:url>', methods=['GET'])
 def process_text(url):   
     # Run a function with the text as an argument
-    #summary = get_summary(url)
+    summary = get_summary(url)
     
     # Return the result as a JSON object
-    return jsonify({'summary': url})
+    return jsonify({'summary': summary})
 
     
 if __name__ == '__main__':
